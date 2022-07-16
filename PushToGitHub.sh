a5d5b1bc8dbd01859317d4b57c0572fa54e1b31f
shanpengnian <shanpengnian@163.com>
2022-07-16 11:08:41 +0800

    dubbo-zookeeper

--- PushToGitHub.sh
+++ PushToGitHub.sh
@@ -8,15 +8,12 @@ git config --global user.name "shanpengnian"
 git config --global user.email "shanpengnian@163.com"
 
 # 提交描述
-msg='springboot-dubbo-zookeeper'
+msg='dubbo-zookeeper'
 
 # 提交仓库地址
-
-githubUrl=https://gitee.com/shanpengnian/springboot-dubbo-zookeeper.git
-
+githubUrl=git@github.com:shanpengnian/springboot-dubbo-zookeeper.git
 
 git init
-# git remote add origin git@github.com:shanpengnian/Sxpcwlkj-Doc.git
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:master # 推送到github master分支
